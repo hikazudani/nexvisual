328e4926b8854b3a7bdef18d42f562dd7e52afb3
Lucas Jundi Hikazudani <lhjundi@proton.me>
2026-02-01 02:09:02 -0300

    feat: Add OnEnable and OnDisable

--- Assets/Script/InitialSetup.cs
+++ Assets/Script/InitialSetup.cs
@@ -3,33 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 
-public class NewBehaviourScript : MonoBehaviour
+public class InitialSetup : MonoBehaviour
 {
 
     [SerializeField] private float requiredArea;
     [SerializeField] private ARPlaneManager planeManager;
     [SerializeField] private GameObject startExperienceUI;
 
-    // Start is called before the first frame update
-    void Start()
+    void OnEnable()
     {
-        
+        planeManager.planesChanged += OnPlanesUpdated;
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnDisable()
     {
+        planeManager.planesChanged -= OnPlanesUpdated;
+    }
+
+
+    public void OnClickStartExperience()
+    {
+        Debug.Log("Initializing the AR Experience...");
+        startExperienceUI.SetActive(false);
+        planeManager.enabled = false;
+
         foreach (var plane in planeManager.trackables)
         {
-            if ( plane.extents.x * plane.extents.y >= requiredArea)
+            plane.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnPlanesUpdated(ARPlanesChangedEventArgs args)
+    {
+        foreach(var plane in args.updated)
+        {
+            if (plane.extents.x * plane.extents.y >= requiredArea)
             {
-                // Found a plane
+                // Plane Found
                 startExperienceUI.SetActive(true);
-                return;
             }
         }
-
-        // no plane found
-        startExperienceUI.SetActive(false);
     }
 }
